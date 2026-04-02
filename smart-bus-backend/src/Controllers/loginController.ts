1e29eda91ba0f5f99787cec9726ba8a31b03e2ea
Retaj Yasser <retagelnashar@gmail.com>
2026-04-02 06:11:46 +0200

    login & jwt

---
+++ smart-bus-backend/src/Controllers/loginController.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+import User from "../models/User";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../utils/generateToken";
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    // check email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email" });
+    }
+
+    // compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+
+    // generate token
+    const token = generateToken(user._id.toString(), user.role);
+
+    res.json({
+      message: "Login success",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        role: user.role,
+      },
+    });
+
+  } catch (err: any) {
+    res.status(500).json({ error: err.message });
+  }
+};
---
+++ smart-bus-backend/src/utils/generateToken.ts
@@ -0,0 +1,9 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (id: string, role: string) => {
+  return jwt.sign(
+    { id, role },
+    process.env.JWT_SECRET as string,
+    { expiresIn: "1d" }
+  );
+};
